97cf8b4237a8b017d288a4fcad173f874399955f
luca5med <lucasmedesilva@gmail.com>
2022-05-24 18:22:58 -0300

    Resolução do projeto final do módulo 1 - Lógica de programação

---
+++ [LucasMedeiros]_projeto1.py
@@ -0,0 +1,165 @@
+import json
+import os.path
+import sys
+
+def obter_dados():
+    '''
+    Essa função carrega os dados dos produtos e retorna uma lista de dicionários, onde cada dicionário representa um produto.
+    NÃO MODIFIQUE essa função.
+    '''
+    with open(os.path.join(sys.path[0], 'dados.json'), 'r') as arq:
+        dados = json.loads(arq.read())
+    return dados
+
+def listar_categorias(dados: list) -> list:
+    '''
+    O parâmetro "dados" deve ser uma lista de dicionários representando os produtos.
+    Essa função deverá retornar uma lista contendo todas as categorias dos diferentes produtos.
+    Cuidado para não retornar categorias repetidas.
+    '''
+
+    categorias = []
+
+    for i in range(0, len(dados)):
+        if not(dados[i]['categoria'].upper() in categorias):
+            categorias.append(dados[i]['categoria'].upper())
+    return sorted(categorias)
+
+def listar_por_categoria(dados: list, categoria: str) -> list:
+    '''
+    O parâmetro "dados" deve ser uma lista de dicionários representando os produtos.
+    O parâmetro "categoria" é uma string contendo o nome de uma categoria.
+    Essa função deverá retornar uma lista contendo todos os produtos pertencentes à categoria dada.
+    '''
+    lista = []
+
+    for i in range(0, len(dados)):
+      if dados[i]['categoria'].upper() == categoria:
+        lista.append(dados[i])
+    return lista
+
+
+def produto_mais_caro(dados: list, categoria: str) -> dict:
+    '''
+    O parâmetro "dados" deve ser uma lista de dicionários representando os produtos.
+    O parâmetro "categoria" é uma string contendo o nome de uma categoria.
+    Essa função deverá retornar um dicionário representando o produto mais caro da categoria dada.
+    '''
+    valor_caro = 0
+
+    for i in range(0, len(dados)):
+
+        if float(dados[i]['preco']) > valor_caro and dados[i]['categoria'].upper() == categoria:
+            valor_caro = float(dados[i]['preco'])
+            produto_caro = dados[i]
+
+    return produto_caro
+
+def produto_mais_barato(dados: list, categoria: str) -> dict:
+    '''
+    O parâmetro "dados" deve ser uma lista de dicionários representando os produtos.
+    O parâmetro "categoria" é uma string contendo o nome de uma categoria.
+    Essa função deverá retornar um dicionário representando o produto mais caro da categoria dada.
+    '''
+    cont = 0
+
+    if dados[0]['categoria'] == categoria:
+        valor_barato = float(dados[0]['preco'])
+
+    while not (dados[cont]['categoria'] == categoria):
+        valor_barato = float(dados[cont]['preco'])
+        cont += 1
+    else:
+        for i in range(0, len(dados)):
+
+            if float(dados[i]['preco']) < valor_barato and dados[i]['categoria'].upper() == categoria:
+                valor_barato = float(dados[i]['preco'])
+                produto_barato = dados[i]
+
+        return produto_barato
+
+def top_10_caros(dados: list) -> list:
+    '''
+    O parâmetro "dados" deve ser uma lista de dicionários representando os produtos.
+    Essa função deverá retornar uma lista de dicionários representando os 10 produtos mais caros.
+    '''
+    lista = sorted(dados, key = lambda x: float(x['preco']), reverse= True)[0:10]
+    return (lista)
+
+def top_10_baratos(dados: list) -> list:
+    '''
+    O parâmetro "dados" deve ser uma lista de dicionários representando os produtos.
+    Essa função deverá retornar uma lista de dicionários representando os 10 produtos mais baratos.
+    '''
+
+    lista = sorted(dados, key = lambda x: float(x['preco']))[0:10]
+    return (lista)
+
+
+def menu(dados: list) -> None:
+    '''
+    O parâmetro "dados" deve ser uma lista de dicionários representando os produtos.
+    Essa função deverá, em loop, realizar as seguintes ações:
+    - Exibir as seguintes opções:
+        1. Listar categorias
+        2. Listar produtos de uma categoria
+        3. Produto mais caro por categoria
+        4. Produto mais barato por categoria
+        5. Top 10 produtos mais caros
+        6. Top 10 produtos mais baratos
+        0. Sair
+    - Ler a opção do usuário.
+    - No caso de opção inválida, imprima uma mensagem de erro.
+    - No caso das opções 2, 3 ou 4, pedir para o usuário digitar a categoria desejada.
+    - Chamar a função adequada para tratar o pedido do usuário e salvar seu retorno.
+    - Imprimir o retorno salvo.
+    O loop encerra quando a opção do usuário for 0.
+    '''
+
+    print('''\nDigite o respectivo número para obter uma opção abaixo:
+        1. Listar categorias
+        2. Listar produtos de uma categoria
+        3. Produto mais caro por categoria
+        4. Produto mais barato por categoria
+        5. Top 10 produtos mais caros
+        6. Top 10 produtos mais baratos
+        0. Sair\n''')
+
+    numero = input('Qual opção você deseja escolher? ')
+    print()
+    opcoes = ['0', '1', '2', '3', '4', '5', '6']
+
+    while not (numero == '0'):
+        while not (numero.isdigit() and numero in opcoes):
+            numero = input('\nVocê digitou uma opção inválida!\nQual opção você deseja escolher? ')
+            print()
+        else:
+            if numero == '1':
+                resposta = listar_categorias(dados)
+            elif numero == '2':
+                categoria = input('\nQual categoria você deseja escolher? ').upper()
+                resposta = listar_por_categoria(dados, categoria)
+            elif numero == '3':
+                categoria = input('\nQual categoria você deseja escolher? ').upper()
+                resposta = produto_mais_caro(dados, categoria)
+            elif numero == '4':
+                categoria = input('\nQual categoria você deseja escolher? ').upper()
+                resposta = produto_mais_barato(dados, categoria)
+            elif numero == '5':
+                resposta = top_10_caros(dados)
+            elif numero == '6':
+                resposta = top_10_baratos(dados)
+
+        if type(resposta) == list:
+            for i in range(0, len(resposta)):
+                print(resposta[i])
+        else:
+            print(resposta)
+
+        print('~*' * 21 + '~')
+        numero = input('\nQual opção você deseja agora? ')
+    print('\nObrigado por usar o nosso programa! FIM!')
+
+2# Programa Principal - não modificar!
+dados = obter_dados()
+menu(dados)
